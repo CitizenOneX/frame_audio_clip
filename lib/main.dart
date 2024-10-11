f7852cc8c94775234d6aa395a9fb4ff2ca81272f
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2024-10-11 15:08:23 +1100

    added raw_sound package (more recent fork) and associated configuration changes for build, plus added simple audio recorder/playback UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:frame_audio_clip/audio_data_response.dart';
 import 'package:logging/logging.dart';
+import 'package:raw_sound/raw_sound_player.dart';
 
 import 'package:simple_frame_app/simple_frame_app.dart';
 import 'package:simple_frame_app/tx/code.dart';
@@ -22,6 +23,9 @@ class MainApp extends StatefulWidget {
 /// SimpleFrameAppState mixin helps to manage the lifecycle of the Frame connection outside of this file
 class MainAppState extends State<MainApp> with SimpleFrameAppState {
   StreamSubscription<Uint8List>? audioClipStreamSubs;
+  final List<Uint8List> _rawAudioClips = [];
+  final _player = RawSoundPlayer();
+  int? _playingIndex;
 
   MainAppState() {
     Logger.root.level = Level.INFO;
@@ -34,14 +38,21 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
   @override
   void initState() {
     super.initState();
+    _player.initialize(nChannels: 1, sampleRate: 8000, pcmType: RawSoundPCMType.PCMI16).then((value) {
+      setState(() {
+        // Trigger rebuild to update UI
+      });
+    });
   }
 
   @override
   void dispose() async {
     await audioClipStreamSubs?.cancel();
+    await _player.release();
     super.dispose();
   }
 
+  /// Start recording audio on Frame
   @override
   Future<void> run() async {
     currentState = ApplicationState.running;
@@ -52,21 +63,15 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
       await audioClipStreamSubs?.cancel();
       audioClipStreamSubs = audioDataResponse(frame!.dataResponse).listen((audioData) {
         _log.info('Clip length: ${audioData.length} bytes');
+        setState(() {
+          _rawAudioClips.add(audioData);
+          currentState = ApplicationState.ready;
+        });
       });
 
       // tell Frame to start streaming audio
       await frame!.sendMessage(TxCode(msgCode: 0x30));
 
-      // wait a while
-      await Future.delayed(const Duration(seconds: 1));
-
-      // tell Frame to stop
-      await frame!.sendMessage(TxCode(msgCode: 0x31));
-
-      // TODO should really block until the audio data has come back so the UI doesn't allow us to kick it off again
-
-      currentState = ApplicationState.ready;
-      if (mounted) setState(() {});
     } catch (e) {
       _log.fine('Error executing application logic: $e');
       currentState = ApplicationState.ready;
@@ -74,35 +79,96 @@ class MainAppState extends State<MainApp> with SimpleFrameAppState {
     }
   }
 
+  /// Stop recording audio on Frame
   @override
   Future<void> cancel() async {
-    // TODO any logic while canceling?
 
-    currentState = ApplicationState.ready;
-    if (mounted) setState(() {});
+    // in canceling state, the user can't click other buttons to start/stop
+    // and the state will return to ready when the audio data is all received
+    setState(() {
+      currentState = ApplicationState.canceling;
+    });
+
+    // tell Frame to stop streaming audio
+    await frame!.sendMessage(TxCode(msgCode: 0x31));
+  }
+
+  /// Play the audio from the selected recording
+  Future<void> _playAudio(Uint8List audioBytes, int index) async {
+    if (!_player.isPlaying) {
+      await _player.play();
+      setState(() {
+        _playingIndex = index;
+      });
+    }
+
+    if (_player.isPlaying) {
+      // TODO skip the pop at the beginning of the recording? or not?
+      //await _player.feed(Uint8List.fromList(audioBytes.skip(2700).toList()));
+      await _player.feed(Uint8List.fromList(audioBytes));
+    }
+
+    // no obvious callback from raw_sound when playback finishes, so since we know
+    // how long the clip is, we can wait then update the UI (show play button instead of stop)
+    // but be aware that the user might have stopped playback early, so bail out
+    int clipDurationMs = (audioBytes.length/16.0).toInt();
+    int waited = 0;
+
+    while (_player.isPlaying && waited < clipDurationMs) {
+      await Future.delayed(const Duration(milliseconds: 100));
+      waited += 100;
+    }
+
+    await _stopAudio();
+  }
+
+  /// Cancel the playing of the selected recording
+  Future<void> _stopAudio() async {
+    if (_player.isPlaying) {
+      await _player.stop();
+      setState(() {
+        _playingIndex = null;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Simple Frame App Template',
+        title: 'Frame Audio Recorder',
         theme: ThemeData.dark(),
         home: Scaffold(
           appBar: AppBar(
-              title: const Text('Simple Frame App Template'),
+              title: const Text('Frame Audio Recorder'),
               actions: [getBatteryWidget()]),
-          body: const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Spacer(),
-              ],
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView.builder(
+              itemCount: _rawAudioClips.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text('Audio Clip ${index + 1} (${(_rawAudioClips[index].length/2/8000.0).toStringAsFixed(2)}s)'),
+                    trailing: _playingIndex == index ?
+                      IconButton(
+                        icon: const Icon(Icons.stop),
+                        onPressed: () {
+                          _stopAudio();
+                        },
+                       ) :
+                      IconButton(
+                        icon: const Icon(Icons.play_arrow),
+                        onPressed: () {
+                          _playAudio(_rawAudioClips[index], index);
+                        },
+                      ),
+                  ),
+                );
+              },
             ),
           ),
           floatingActionButton: getFloatingActionButtonWidget(
-              const Icon(Icons.file_open), const Icon(Icons.close)),
+              const Icon(Icons.mic), const Icon(Icons.stop)),
           persistentFooterButtons: getFooterButtonsWidget(),
         ));
   }
